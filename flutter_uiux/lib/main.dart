487bbd097749815af6c9732dcdcbfc42e65929a4
shad <shadislam999@gmail.com>
2024-02-20 15:53:49 +0600

    tab able funtion and complate

--- flutter_uiux/lib/main.dart
+++ flutter_uiux/lib/main.dart
@@ -10,66 +10,32 @@ void main() {
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
-
-
-
-List names = ['shad', 'pro', 'noob'];
+  //function and method 
+  void userTab(){
+    print('user tapped');
+  }
 
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        //backgroundColor: Colors.purple[200],
-       //in this section  appbar menu we add icon using leading button
-       //more icon we use action[] the in action
-      /*  appBar: AppBar(
-          backgroundColor: Colors.deepPurple,
-          title: Text('My App Bar'),
-          elevation: 0,
-          leading:Icon(Icons.menu) ,
-          actions: [
-            IconButton(
-              onPressed: (){},
-               icon: Icon(Icons.logout),
-               ),
-          ],
-        ),
-        */
-        //list veiw is scrollable and much batter
-        body:GridView.builder(
-          itemCount: 64,
-          gridDelegate:
-          //how many rows
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-           itemBuilder: (context, index)=>Container(
-            color: Colors.purple,
-            margin: EdgeInsets.all(2),
-           )),
-
-
-        /* ListView(
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            Container(
-              color: Colors.deepPurple[200],
-              width: 350,
-             
+       
+        
+      body: Center(
+        child: GestureDetector(
+          onTap:userTab,
+          child: Container(
+           
+            
+          width: 200,
+          height: 200,
+          color: Colors.purple,
+          child: Center(child: Text("Tap me")),
             ),
-
-             Container(
-               color: Colors.deepPurple,
-               width: 350,
-             
-             ),
-              Container(
-               color: Colors.deepPurple[300],
-               width: 350,
-               
-              ),
-          ]
-          ),*/
-          
+        ),
+      ),
+      
         ),
       );
     
